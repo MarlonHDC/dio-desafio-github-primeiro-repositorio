e46815f8484315d3e37e92048e4f3c39f135f9c4
origin <https://github.com/MarlonHDC/teste-curso-dio-dominando-ides-java-intellij.git>
2021-12-16 14:42:39 -0300

    Entradas e Saídas 2.0

--- Entradas e Saídas Java/src/EntradasSaidasJava.java
+++ Entradas e Saídas Java/src/EntradasSaidasJava.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; // Comando criado automaticamente clicando em "Scanner" e importando a biblioteca.
+
 public class EntradasSaidasJava {
 
 
@@ -6,7 +8,16 @@ public class EntradasSaidasJava {
         String nomesaida = "Gustavo";
         float notasaida = 85.f;
         System.out.println("A nota é " + notasaida);
-        System.out.printf("A nota de %s é %.4f \n", nomesaida, notasaida);
-        System.out.format("A nota de %s é %.2f \n", nomesaida, notasaida);
+        System.out.printf("A nota de %s é %.4f \n", nomesaida, notasaida); // O parâmetro "%s" refere-se a String "nomesaida"...
+        System.out.format("A nota de %s é %.2f \n", nomesaida, notasaida); // O parâmetro "%.4f" refere-se ao número de casas decimais;
+
+        // Algumas maneiras de formatar as entradas no Scanner util system
+        Scanner teclado = new Scanner(System.in);   // Este comando força a importação da biblioteca classe "JAVA.util.scanner";
+        System.out.print("\nDigite o nome do aluno: "); // Na biblioteca de Scanner os parâmetros na entrada...
+        String nomeentrada = teclado.nextLine();
+        System.out.print("Digite a nota do aluno: "); //... não precisa criar os parâmetros primitivos antes, estes são criados quando digitados;
+        Float notaentrada = teclado.nextFloat();
+        System.out.format("A nota de %s é %.2f \n" , nomeentrada, notaentrada);
+
     }
 }
